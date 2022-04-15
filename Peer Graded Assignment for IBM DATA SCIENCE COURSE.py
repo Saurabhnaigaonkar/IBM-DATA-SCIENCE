8622cad47227152f7eaba3e76571c14ea74cf8ae
Saurabh Naigaonkar <81736981+Saurabhnaigaonkar@users.noreply.github.com>
2022-04-15 12:39:07 +0530

    Add files via upload

---
+++ Peer Graded Assignment for IBM DATA SCIENCE COURSE.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <H1> My Jupyter Notebook on IBM Watson Studio </H1>
+
+# Saurabh Satish Naigaonkar
+# Software Engineer
+
+# <I> I am Intrested in Data Science because I would love to contribute towards affordable and quality solutions in the future. </I> 
+
+# <H3> The below script should print addition of two numbers : </H3>
+
+# In[8]:
+
+
+def sum(a,b):
+    addition_of_two_numbers = a + b;
+    return addition_of_two_numbers;
+a = int(input("enter a 1st number : "))
+b = int(input("enter a 2nd number : "))
+print("Addition of two numbers is : ",sum(a,b))
+
+
+# In[ ]:
+
+
+
+
